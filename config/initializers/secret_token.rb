009958a6e389c691a6bc2d02c762f16abe98dffa
Marcin Stecki <marcin@netguru.pl>
2013-12-16 23:01:10 +0100

    simple ror app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FeatureFlagsExample::Application.config.secret_key_base = '9616899e30a382c19bc51a35ca123b1e35bf8ba7c524a576c39c8efff351e16cd79bc820ba0786ea87eaa908372730809129f72180b1506fb9ea0277c21a1bbb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FeatureFlagsExample::Application.config.session_store :cookie_store, key: '_feature_flags_example_session'
