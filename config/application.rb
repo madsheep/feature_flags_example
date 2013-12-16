a68c2cfb8391ebb17a94935389e8c78755c71d4b
Marcin Stecki <marcin@netguru.pl>
2013-12-16 23:09:20 +0100

    add features setup

--- config/application.rb
+++ config/application.rb
@@ -6,18 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+FeatureFlags = Konf.new("config/features.yml")[Rails.env]
+EnabledFeatures = FeatureFlags.map{|feature, enabled| feature if enabled }.compact
+
 module FeatureFlagsExample
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.autoload_paths += EnabledFeatures.map{|f| "features/#{f}/lib" }
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.before_initialize do
+      EnabledFeatures.each do |feature|
+        feature_engine = feature.camelcase.constantize::Engine
+        feature_engine.instance.run_initializers :default, self
+      end
+    end
+
   end
 end
